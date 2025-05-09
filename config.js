61e9b828ae2729b42a2ff4df0cfdbdcd7df94e89
Tymphaei <leonardo.7lop7@gmail.com>
2025-05-08 18:00:49 -0600

    Add Instructive Page

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const DATABASE_HOST = process.env.DATABASE_HOST || 'localhost'
 const DATABASE_USERNAME = process.env.DATABASE_USERNAME || 'root'
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || 'Mayelon7'
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || 'Zethus001'
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'itaih_datospersonales'
 const DATABASE_PORT = process.env.DATABASE_PORT || 3306;
 
